545c08ede7bf56d1c50a28937bd6375c0f16b252
Zhou Taiyu <129917363+LightningYu@users.noreply.github.com>
2026-02-22 19:36:10 +0800

    Revert "Fix media notification play button on Android 14+ (API 34+)"

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
@@ -7,7 +7,6 @@ import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ServiceInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
@@ -725,12 +724,7 @@ public class AudioService extends MediaBrowserServiceCompat {
     }
 
     private void internalStartForeground() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ServiceCompat.startForeground(this, NOTIFICATION_ID, buildNotification(),
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK);
-        } else {
-            startForeground(NOTIFICATION_ID, buildNotification());
-        }
+        startForeground(NOTIFICATION_ID, buildNotification());
         notificationCreated = true;
     }
 
